05ca365daab7d0148947b65e94819c5fd47699df
Iscander <zIscander@yandex.ru>
2023-02-06 23:47:20 +0500

    Готов
    Доделал сайт версия 1

--- Stellar/js/click.js
+++ Stellar/js/click.js
@@ -1,12 +1,12 @@
 // Анимация нажатия на кнопку меню
 $('.mini-nav-btn').on('click', function(e){
 	e.preventDefault;
-	$(this).toggleClass('mini-nav-btn-active');
-	$('.mini-nav').toggleClass('mini-nav-active');
+	$('.mini-nav').toggleClass('mini-nav_active');
+
 });
+
 // Анимация по нажатию на меню
 // на ссылку и меню уходит в кнопку
 $('.mini-nav-link').on('click', function(e) {
-	$('.mini-nav').toggleClass('mini-nav-active');
-	$('.mini-nav-btn').toggleClass('mini-nav-btn-active');
+	$('.mini-nav').toggleClass('mini-nav_active');
 });
