09011a5cd5c95e62cf0c5dc6871797461364d2b1
Iscander <117932482+zIscander@users.noreply.github.com>
2023-04-16 16:31:59 +0500

    Update footer.php

--- garant/footer.php
+++ garant/footer.php
@@ -4,11 +4,11 @@
 			<div class="row">
 				<div class="col-2">
 					<ul class="footer-list">
-						<li class="footer-list__item"><a class="footer__link" href="#">«ГАРАНТ РИЕЛТОР»</a></li>
-						<li class="footer-list__item"><a class="footer__link" href="#">О компании</a></li>
-						<li class="footer-list__item"><a class="footer__link" href="#">Услуги</a></li>
-						<li class="footer-list__item"><a class="footer__link" href="#">Контакты</a></li>
-						<li class="footer-list__item"><a class="footer__link" href="#">Каталог предложений</a></li>
+						<li class="footer-list__item"><a class="footer__link" href="index.php">«ГАРАНТ РИЕЛТОР»</a></li>
+						<li class="footer-list__item"><a class="footer__link" href="about.php">О компании</a></li>
+						<li class="footer-list__item"><a class="footer__link" href="services.php">Услуги</a></li>
+						<li class="footer-list__item"><a class="footer__link" href="contacts.php">Контакты</a></li>
+						<li class="footer-list__item"><a class="footer__link" href="ads.php">Каталог предложений</a></li>
 					</ul>
 				</div>
 				<div class="col-8">
@@ -20,8 +20,8 @@
 				</div>
 				<div class="col-2">
 					<div class="footer__block">
-						<p class="footer__p"><a class="footer__link" href="tel:">8 (912) 291 39 17</a></p>
-						<p class="footer__p">Краснотурьинск, ул. Татарараева 19 - 2</p>
+						<p class="footer__p"><a class="footer__link" href="tel:83438465600">8 (34384) 6-56-00</a></p>
+						<p class="footer__p">г. Краснотурьинск, Ленина 42</p>
 					</div>
 				</div>
 			</div>
