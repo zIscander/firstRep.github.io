27d346eee4f2e61de6039648b2ad5771df41016a
Iscander <117932482+zIscander@users.noreply.github.com>
2024-02-16 23:43:50 +0500

    Delete garant directory

--- garant/js/click.js
+++
@@ -1,9 +0,0 @@
-// Анимация нажатия на кнопку меню
-$('.nav__btn').on('click', function(e){
-	e.preventDefault;
-	$('.nav').toggleClass('nav_active');
-	$('.nav__btn').toggleClass('nav__btn_active');
-	$('.nav__span').toggleClass('nav__span_active');
-	$('.nav-link').toggleClass('nav-link_active');
-
-});
